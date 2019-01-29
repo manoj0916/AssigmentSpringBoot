de27841248e8b3b21e26984eab1f464f89c01dc4
Manoj <k.manoj.panda@gmail.com>
2019-01-29 02:57:17 +0000

    Clean up

--- ProductRESTService/src/main/java/com/jlp/application/config/ProductServiceConfiguration.java
+++ ProductRESTService/src/main/java/com/jlp/application/config/ProductServiceConfiguration.java
@@ -1,11 +1,8 @@
 package com.jlp.application.config;
 
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 /**
  * @author Manoj
@@ -15,13 +12,4 @@ import org.springframework.context.support.ReloadableResourceBundleMessageSource
 @PropertySource("classpath:resources/application.properties")
 @ImportResource("classpath:resources/applicationContext.xml")
 public class ProductServiceConfiguration {
-	/*@Bean
-	public MessageSource messageSource() {
-	    ReloadableResourceBundleMessageSource messageSource
-	      = new ReloadableResourceBundleMessageSource();
-	     
-	    messageSource.setBasename("classpath:messages/messages");
-	    messageSource.setDefaultEncoding("UTF-8");
-	    return messageSource;
-	}*/
 }
